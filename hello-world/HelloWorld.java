fefbfe67500d4df2c8b9128d378edc00b28b640b
Ratan Singh Kalakoti <116471445+ratank76@users.noreply.github.com>
2022-11-19 08:25:09 +0530

    testing post script region date and time at host
    testing post script region date and time at host

--- hello-world/HelloWorld.java
+++ hello-world/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 		system.out.println("Hello, Jenkins");
 		system.out.println("Hello, Jenkins Poll SCM");
 		system.out.println("Hello, testing Build 5 using Poll SCM");
+		system.out.println("Hello, testing post script region date and time at host");
 
 	}
 
