b5e55237ab992a464a74943a855732d07cedb647
Ratan Singh Kalakoti <116471445+ratank76@users.noreply.github.com>
2022-11-19 08:33:01 +0530

    Deleted extra lines
    Deleted extra lines

--- hello-world/HelloWorld.java
+++ hello-world/HelloWorld.java
@@ -4,10 +4,7 @@ public class HelloWorld {
 
 		system.out.println("Hello, World");
 		system.out.println("Hello, Jenkins");
-		system.out.println("Hello, Jenkins Poll SCM");
-		system.out.println("Hello, testing Build 5 using Poll SCM");
-		system.out.println("Hello, testing post script region date and time at host");
-
+		
 	}
 
 }
